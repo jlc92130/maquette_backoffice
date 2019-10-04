8cfef4c206a3722a141e5ad4c1c7fbe8fddb2ce7
jl <jl.crobeddu@codeur.online>
2019-10-04 17:00:59 +0200

    modif header

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="fr" dir="ltr">
 <head>
     <meta charset="utf-8">
-    <meta name="author" lang="fr" content="Eddy MORLON, Thomas Gallusser">
+    <meta name="author" lang="fr" content="jlc">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>StartupRR</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
